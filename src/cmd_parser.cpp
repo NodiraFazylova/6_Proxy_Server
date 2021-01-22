b68e422c4f808695167cb30a3de14d504a6e45ec
NodiraFazylova <nshorakhmedova@gmail.com>
2021-01-23 03:37:21 +0500

    Wrapp the command line parser in the namespace

---
+++ src/cmd_parser.cpp
@@ -0,0 +1,8 @@
+#include "cmd_parser.hpp"
+
+namespace proxy_server_6
+{
+
+#include "3rdparty/cmd_parser/CommandLine.ipp"
+
+}   // namespace proxy_server_6
---
+++ src/cmd_parser.hpp
@@ -0,0 +1,13 @@
+#ifndef CMD_LINE_PARSER_HPP
+#define CMD_LINE_PARSER_HPP
+
+namespace proxy_server_6
+{
+
+#include "3rdparty/cmd_parser/CommandLIne.hpp"
+
+using cmd_parser = CommandLine;
+
+}   // namespace proxy_server_6
+
+#endif  /* CMD_LINE_PARSER */
