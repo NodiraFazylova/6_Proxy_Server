19808bb52359d03056f820260f07c3aad02dfc2e
NodiraFazylova <nshorakhmedova@gmail.com>
2021-01-23 20:02:02 +0500

    Update cmd parser

--- src/cmd_parser.cpp
+++ src/cmd_parser.cpp
@@ -1,8 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <variant>
+#include <vector>
+
+#include "logger.h"
 #include "cmd_parser.hpp"
+#include "server.hpp"
 
 namespace proxy_server_6
 {
 
-#include "3rdparty/cmd_parser/CommandLine.ipp"
+#include "cmd_parser/CommandLine.ipp"
+
+cmd_parser make_cmd_parser( proxy_server_6::server::config_t & config, bool & help_opt )
+{
+    cmd_parser parser;
+    parser.addArgument( { "-d", "--dir" }, &config.dir_path, "Path to the directory where files will be stored" );
+    parser.addArgument( { "-i", "--ip" }, &config.dir_path, "IP address of server listener" );
+    parser.addArgument( { "-p", "--port" }, &config.dir_path, "Port of server listener" );
+    parser.addArgument( { "-m", "--maxdata" }, &config.dir_path, "The amount of data that the server can store per client" );
+    parser.addArgument( { "-w", "--workers" }, &config.dir_path, "Maximum number of available cache tables" );
+    parser.addArgument( { "-v", "--verbose" }, &config.dir_path, "Flag that indicates that debug messages is printed to stdout (stderr), if not set server prints only errors" );
+    parser.addArgument( { "-h", "?", "--help" }, &config.dir_path, "Print help string" );
+
+    return parser;
+}
 
 }   // namespace proxy_server_6
--- src/cmd_parser.hpp
+++ src/cmd_parser.hpp
@@ -1,12 +1,26 @@
 #ifndef CMD_LINE_PARSER_HPP
 #define CMD_LINE_PARSER_HPP
 
+#include <iostream>
+#include <string>
+#include <variant>
+#include <vector>
+
+#include "server.hpp"
+
+/**
+ * Slightly adapted command line parser
+ * http://schneegans.github.io/tutorials/2019/08/06/commandline 
+ */
+
 namespace proxy_server_6
 {
 
-#include "3rdparty/cmd_parser/CommandLIne.hpp"
+#include "cmd_parser/CommandLine.hpp"
 
 using cmd_parser = CommandLine;
+
+cmd_parser make_cmd_parser( proxy_server_6::server::config_t & config, bool & help_opt);
 
 }   // namespace proxy_server_6
 
