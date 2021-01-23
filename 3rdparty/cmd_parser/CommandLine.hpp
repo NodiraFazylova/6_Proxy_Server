83e71a4c417b2d0dfd62fd00cf62392c9395bec2
NodiraFazylova <nshorakhmedova@gmail.com>
2021-01-23 19:56:48 +0500

    Fixed parser

--- 3rdparty/cmd_parser/CommandLine.hpp
+++ 3rdparty/cmd_parser/CommandLine.hpp
@@ -1,11 +1,6 @@
 #ifndef COMMAND_LINE_HPP
 #define COMMAND_LINE_HPP
 
-#include <iostream>
-#include <string>
-#include <variant>
-#include <vector>
-
 // This class is a simple and effective class to parse command line arguments.
 // For each possible argument it stores a pointer to a variable. When the
 // corresponding argument is set on the command line (given to the parse()
@@ -26,7 +21,8 @@
 // --help=false
 // --help true
 
-class CommandLine {
+class CommandLine
+{
 public:
 
     // These are the possible variables the options may point to. Bool and
@@ -34,43 +30,45 @@ public:
     // with a std::stringstream. This std::variant can be easily extended if
     // the stream operator>> is overloaded. If not, you have to add a special
     // case to the parse() method.
-    typedef std::variant<int32_t*,
-        uint32_t*,
-        double*,
-        float*,
-        bool*,
-        std::string*> Value;
+    typedef std::variant<int32_t *,
+        uint32_t *,
+        int64_t *,
+        double *,
+        float *,
+        bool *,
+        std::string *> Value;
 
     // The description is printed as part of the help message.
-    explicit CommandLine(std::string description);
+    explicit CommandLine() = default;
 
     // Adds a possible option. A typical call would be like this:
     // bool printHelp = false;
     // cmd.addArgument({"--help", "-h"}, &printHelp, "Print this help message");
     // Then, after parse() has been called, printHelp will be true if the user
     // provided the flag.
-    void addArgument(std::vector<std::string> const& flags,
-        Value const& value, std::string const& help);
+    void addArgument( std::vector<std::string> const & flags,
+                      Value const & value, std::string const & help );
 
     // Prints the description given to the constructor and the help
     // for each option.
-    void printHelp(std::ostream& os = std::cout) const;
+    void printHelp( std::ostream & os = std::cout ) const;
 
     // The command line arguments are traversed from start to end. That means,
     // if an option is set multiple times, the last will be the one which is
     // finally used. This call will throw a std::runtime_error if a value is
     // missing for a given option. Unknown flags will cause a warning on
     // std::cerr.
-    void parse(int argc, char* argv[]) const;
+    void parse( int argc, char * argv[] ) const;
 
 private:
-    struct Argument {
+    struct Argument
+    {
         std::vector<std::string> mFlags;
         Value                    mValue;
         std::string              mHelp;
     };
 
-    std::string           mDescription;
+    mutable std::string   mDescription;
     std::vector<Argument> mArguments;
 };
 
