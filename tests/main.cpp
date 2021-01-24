ed3571be8ce82784e3bb400206d25c26eba03573
NodiraFazylova <nshorakhmedova@gmail.com>
2021-01-24 13:06:30 +0500

    Update parser test cases

--- tests/main.cpp
+++ tests/main.cpp
@@ -32,9 +32,48 @@ TEST_CASE( "Test command line arguments parser:", "[cmd_parser]" )
 
         parser.parse( argc, argv );
         
-        REQUIRE_FALSE( help_opt );
+        REQUIRE( help_opt == true );
 
         bool is_equal = config == proxy_server_6::default_config;
         REQUIRE( is_equal == true );
+    }
+
+    SECTION( "all invalid argv:" )
+    {
+        char * argv[] = { "tests", "qwerty", "qwerty" };
+        int    argc = 3;
+
+        parser.parse( argc, argv );
+
+        REQUIRE( help_opt == true );
+
+        bool is_equal = config == proxy_server_6::default_config;
+        REQUIRE( is_equal == true );
+    }
+
+    SECTION( "one invalid argv:" )
+    {
+        char * argv[] = { "tests", "--ip=127.0.0.1", "--port=125", "qwerty" };
+        int    argc = 4;
+
+        parser.parse( argc, argv );
+
+        REQUIRE( help_opt == true );
+
+        bool is_equal = config == proxy_server_6::default_config;
+        REQUIRE_FALSE( is_equal );
+    }
+
+    SECTION( "valid argv:" )
+    {
+        char * argv[] = { "tests", "--ip=127.0.0.1", "--port=125" };
+        int    argc = 3;
+
+        parser.parse( argc, argv );
+
+        REQUIRE_FALSE( help_opt );
+
+        bool is_equal = config == proxy_server_6::default_config;
+        REQUIRE_FALSE( is_equal );
     }
 }
