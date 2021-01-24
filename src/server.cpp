efcaf4decab4f921e166dff0484aebda2e14ffbb
NodiraFazylova <nshorakhmedova@gmail.com>
2021-01-24 13:57:45 +0500

    Add config validator

--- src/server.cpp
+++ src/server.cpp
@@ -1,17 +1,17 @@
 #include "logger.h"
 #include "server.hpp"
+#include "utils/is_valid_config.hpp"
 
 namespace proxy_server_6
 {
 
-bool operator== ( const server::config_t & lh, const server::config_t & rh )
+bool is_valid_config( proxy_server_6::server::config_t & config )
 {
-    return  lh.host          == rh.host             &&
-            lh.port          == rh.port             &&
-            lh.dir_path      == rh.dir_path         &&
-            lh.maxdata_size  == rh.maxdata_size     &&
-            lh.workers_count == rh.workers_count    &&
-            lh.verbose       == rh.verbose;
+    return  proxy_server_6::is_valid_host( config.host )                    &&
+            proxy_server_6::is_valid_port( config.port )                    &&
+            proxy_server_6::is_valid_dir_path( config.dir_path )            &&
+            proxy_server_6::is_valid_maxdata_size( config.maxdata_size )    &&
+            proxy_server_6::is_valid_workers_count( config.workers_count );
 }
 
 }   // namespace proxy_server_6
