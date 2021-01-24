245dae009ae94e54924d3648dbc5f2dfa6336c1f
NodiraFazylova <nshorakhmedova@gmail.com>
2021-01-24 18:01:08 +0500

    Add main server interface

--- src/server.cpp
+++ src/server.cpp
@@ -1,9 +1,90 @@
+#include <iostream>
+#include <iomanip>
+#include <utility>
+
 #include "logger.h"
 #include "server.hpp"
 #include "utils/is_valid_config.hpp"
+#include "utils/run_io_context.hpp"
 
 namespace proxy_server_6
 {
+
+server::server( server::config_t config )
+    : m_threads()
+    , m_io_context()
+    , m_io_context_work()
+    , m_host( std::move( config.host ) )
+    , m_port( std::move( config.port ) )
+    , m_dir_path( std::move( config.dir_path ) )
+    , m_maxdata_size( config.maxdata_size )
+    , m_workers_count( config.workers_count )
+    , m_verbose( config.verbose )
+{
+    // setw(10) - length "dir_path"
+    LOG_IF( m_verbose, std::cout, "proxy_server::server::" <<  __FUNCTION__ 
+            << "() - success with config:\n"
+            << std::left << std::setw( 10 ) << "ip: "       << m_host                      << ",\n"
+            << std::left << std::setw( 10 ) << "port: "     << m_port                      << ",\n"
+            << std::left << std::setw( 10 ) << "dir_path: " << m_dir_path                  << ",\n"
+            << std::left << std::setw( 10 ) << "maxdata: "  << m_maxdata_size              << ",\n"
+            << std::left << std::setw( 10 ) << "workers: "  << m_workers_count             << ",\n"
+            << std::left << std::setw( 10 ) << "verbose: "  << std::boolalpha << m_verbose
+    );
+}
+
+
+server::~server()
+{
+    LOG_IF( m_verbose, std::cout, "proxy_server::server::" << __FUNCTION__
+            << "() - BEGIN"
+    );
+
+    stop();
+
+    LOG_IF( m_verbose, std::cout, "proxy_server::server::" << __FUNCTION__
+            << "() - END"
+    );
+}
+
+
+void server::start()
+{
+    LOG_IF( m_verbose, std::cout, "proxy_server::server::" << __FUNCTION__
+            << "()"
+    );
+
+    m_io_context_work.emplace( boost::asio::make_work_guard( m_io_context ) );
+    for( int64_t i = 0; i < m_workers_count; ++i )
+    {
+        m_threads.create_thread( proxy_server_6::run_io_context( m_io_context ) );
+    }
+}
+
+
+void server::stop()
+{
+    LOG_IF( m_verbose, std::cout, "proxy_server::server::" << __FUNCTION__
+            << "() - BEGIN"
+    );
+
+    LOG_IF( m_verbose, std::cout, "proxy_server::server::" << __FUNCTION__
+            << "() - stop io_context"
+    );
+
+    m_io_context.stop();
+
+    LOG_IF( m_verbose, std::cout, "proxy_server::server::" << __FUNCTION__
+            << "() - join threads"
+    );
+
+    m_threads.join_all();
+
+    LOG_IF( m_verbose, std::cout, "proxy_server::server::" << __FUNCTION__
+            << "() - END"
+    );
+}
+
 
 bool is_valid_config( proxy_server_6::server::config_t & config )
 {
--- src/server.hpp
+++ src/server.hpp
@@ -1,8 +1,13 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
+#include <optional>
 #include <stdint.h>
 #include <string>
+
+#include <boost/asio.hpp>
+#include <boost/asio/executor_work_guard.hpp>
+#include <boost/thread.hpp>
 
 namespace proxy_server_6
 {
@@ -19,6 +24,35 @@ public:
         int64_t     workers_count = 8;
         bool        verbose = false;
     };
+
+public:
+    server( config_t config );
+
+    ~server();
+
+private:
+    server(const proxy_server_6::server & ) = delete;
+
+    server & operator= ( const server & ) = delete;
+
+public:
+    void start();
+
+    void stop();
+
+private:
+    using asio_work_guard_t = boost::asio::executor_work_guard<boost::asio::io_context::executor_type>;
+
+    boost::thread_group                 m_threads;
+    boost::asio::io_context             m_io_context;
+    std::optional<asio_work_guard_t>    m_io_context_work;
+
+    std::string                         m_host;
+    std::string                         m_port;
+    std::string                         m_dir_path;
+    int64_t                             m_maxdata_size;
+    int64_t                             m_workers_count;
+    bool                                m_verbose;
 };
 
 bool is_valid_config( proxy_server_6::server::config_t & config );
