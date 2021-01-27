9940f27c5922ba2e4adfe100f6fb92ab29315b2d
NodiraFazylova <nshorakhmedova@gmail.com>
2021-01-27 22:41:59 +0500

    Update src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,24 @@
+#include <iostream>
+
 #include "cmd_parser.hpp"
 #include "logger.h"
 #include "server.hpp"
 
-int main(int argc, char **argv)
+int main( int argc, char ** argv )
 {
-    bool help_opt = false;
-    proxy_server_6::server::config_t config;
-    proxy_server_6::cmd_parser parser = proxy_server_6::make_cmd_parser(config, help_opt);
+    proxy_server_6::server::config_t    config;
+    bool                                help_opt = false;
+    proxy_server_6::cmd_parser          parser = proxy_server_6::make_cmd_parser( config, help_opt );
 
-    parser.parse(argc, argv);
-    if( help_opt /*|| !proxy_server_6::is_valid_config(config)*/ )
+    if( help_opt || !proxy_server_6::is_valid_config( config ) )
     {
         parser.printHelp();
+        return 0;
     }
 
-    proxy_server_6::server server;
+    proxy_server_6::server server( config );
+
+    server.start();
 
     return 0;
 }
