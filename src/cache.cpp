f7fde348a6ac88d8b9bec9b3b2686ccf9a5fd151
NodiraFazylova <nshorakhmedova@gmail.com>
2021-03-16 00:34:40 +0500

    Update cache with pimpl idiom

--- src/cache.cpp
+++ src/cache.cpp
@@ -1,193 +1,170 @@
-#include <set>
+#include <atomic>
+#include <chrono>
+#include <iterator>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <string>
 #include <vector>
+#include <utility>
 
 #include <boost/asio.hpp>
 #include <boost/asio/post.hpp>
 
 #include "cache.hpp"
+#include "detail/bucket.hpp"
+#include "detail/command_queue.hpp"
 #include "logger.h"
 #include "utils/cache_locker.hpp"
 
 namespace proxy_server_6
 {
 
-class command_queue
+class cache::cache_ipml
 {
+
 public:
-    struct timed_command
-    {
-        std::chrono::system_clock::time_point time;
-        size_t                                command_hash;
-    };
-
-    command_queue() = default;
-
-    auto find_by_command(const size_t command_hash)
-    {
-        auto pos = std::begin(m_queue);
-        for( auto end_pos = std::end(m_queue); pos != end_pos; pos = std::next(pos) )
-        {
-            if(pos->command_hash == command_hash)
-            {
-                break;
-            }
-        }
-
-        return pos;
-    }
-
-    void insert(timed_command command)
-    {
-        m_queue.emplace(std::move(command));
-    }
-
-    std::mutex & get_mutex()
-    {
-        return m_mtx;
-    }
-
-    bool empty()
-    {
-        return m_queue.empty();
-    }
-
-    size_t get_front_command()
-    {
-        return m_queue.begin()->command_hash;
-    }
-
-    void delete_front_command()
-    {
-        m_queue.erase(m_queue.begin());
-    }
-
-private:
-
-    std::mutex              m_mtx;
-    std::set<timed_command> m_queue;
-};
-
-
-inline
-bool operator< (const command_queue::timed_command & lh, const command_queue::timed_command & rh)
-{
-    return  (lh.time < rh.time) ||
-            ((lh.time == rh.time) && (lh.command_hash < rh.command_hash));
-}
-
-
-explicit cache::cache( boost::asio::io_context & io_context, size_t bucket_count = 5, std::size_t max_data_size = 1024, bool verbose = false )
-    : m_io_context( io_context )
-    , m_cur_size( 0 )
-    , m_max_size( max_data_size )
-    , m_data( bucket_count )
-    , m_command_by_time()
-    , m_verbose( verbose )
-{
-    LOG_IF( m_verbose,
-            std::cout,
-            "cache::cache()"
-    );
-}
-
-
-std::vector<std::string> cache::get_cached_files()
-{
-    LOG_IF( m_verbose,
-            std::cout,
-            "cache::get_cached_files() - BEGIN"
-    );
-
-    std::vector<std::string> result;
-
-    for( auto & bckt : m_data )
+    
+    explicit cache_impl( boost::asio::io_context & io_context, size_t bucket_count = 5, std::size_t max_data_size = 1024, bool verbose = false )
+        : m_io_context( io_context )
+        , m_cur_size( 0 )
+        , m_max_size( max_data_size )
+        , m_data( data_count )
+        , m_comand_by_time()
+        , m_verbose( verbose )
     {
         LOG_IF( m_verbose,
                 std::cout,
-                "cache::get_cached_files() - add next bucket to result"
+                "cache::cache()"
         );
-        /* cache_locker does not necessarily block the desired mutex, and here we need a guaranteed block, so we use std::lock_guard */
-        std::lock_guard cache_guard( bckt.m_mtx );
-        for( const auto & [key, value] : bckt.m_files_by_command )
-        {
-            result.emplace_back( value );
-        }
     }
 
-    LOG_IF( m_verbose,
-            std::cout,
-            "cache::get_cached_files() - END"
-    );
 
-    return result;
-}
-
-
-void cache::insert( const std::string & command, const std::string & file )
-{
-    size_t command_hash = std::hash<std::string>{}(command);
-    size_t index        = command_hash % m_data.size();
-
-    if( m_cur_size.load( std::memory_order_acquire ) + file.size() > m_max_size )
+    std::vector<std::string> get_cached_files()
     {
-        proxy_server_6::cache_locker data_locker{ m_data[index].m_mtx };
-        proxy_server_6::cache_locker queue_locker{ m_command_by_time.get_mutex() };
-        if( data_locker && queue_locker )
+        LOG_IF( m_verbose,
+                std::cout,
+                "cache::get_cached_files() - BEGIN"
+        );
+
+        std::vector<std::string> result;
+
+        for( auto & bckt : m_data )
         {
-            m_command_by_time.insert( { std::chrono::system_clock::now(), command_hash } ); 
-            m_data[index].m_files_by_command.insert( { command_hash, file } );
+            LOG_IF( m_verbose,
+                    std::cout,
+                    "cache::get_cached_files() - add next bucket to result"
+            );
+
+            /* cache_locker does not necessarily block the desired mutex, and here we need a guaranteed block, so we use std::lock_guard */
+            std::lock_guard cache_guard( bckt.m_mtx );
+            for( const auto & [key, value] : bckt.m_files_by_command )
+            {
+                result.emplace_back( value );
+            }
+        }
+
+        LOG_IF( m_verbose,
+                std::cout,
+                "cache::get_cached_files() - END"
+        );
+
+        return result;
+    }
+
+
+    void insert_file( const std::string & command, const std::string & file )
+    {
+        size_t command_hash = std::hash<std::string>{}(command);
+        size_t index = command_hash % m_data.size();
+
+        if( m_cur_size.load( std::memory_order_acquire ) + file.size() > m_max_size )
+        {
+            std::lock_guard data_locker{ m_data[index].get_mutex() };
+            std::lock_guard queue_locker{ m_command_by_time.get_mutex() };
+
+            m_command_by_time.insert( { std::chrono::system_clock::now(), command_hash } );
+            m_data[index].insert( { command_hash, file } );
             m_cur_size.store( m_cur_size.load( std::memory_order_acquire ) + file.size() );
         }
         else
         {
-            m_io_context.post( std::bind( &cache::insert, this, command, file ) );
+            m_io_context.post( std::bind( &cache_impl::delete_oldest_file, this, command, file ) );
         }
     }
-    else
+
+
+    void delete_oldest_file( const std::string & command, const std::string & file )
     {
-        m_io_context.post( std::bind( &cache::delete_oldest_and_insert, this, command, file ) );
-    }       
+        std::chrono::system_clock::time_point min_time = std::chrono::system_clock::now();
+        std::lock_guard queue_locker{ m_command_by_time.get_mutex() };
+        
+        size_t oldest_command;
+        size_t insex;
+        if( !m_command_by_time.empty() )
+        {
+            oldest_command  = m_command_by_time.get_front_command();
+            index           = oldest_command % m_data.size();
+            m_command_by_time.delete_front_command();
+        }
+        
+        std::lock_guard data_locker{ m_data[oldest_command].get_mutex() };
+        auto & bucket     = m_data[index];
+        const auto & file = bucket.get_file( oldest_command );
+
+        m_cur_size.store( m_cur_size.load( std::memory_order_acquire ) - file.size() )
+        bucket.m_files_by_command.erase( oldest_command );
+
+       
+
+        m_io_context.post( std::bind( &cache_impl::insert_file, this, command, file ) );
+    }
+
+
+    std::string get_file( const std::string & command )
+    {
+        size_t command_hash = std::hash<std::string>{}(command);
+        size_t index = command_hash % m_data.size();
+
+        std::lock_guard locker{ m_data[index].get_mutex() };
+        
+        return m_data[index].m_files_by_command[command_hash];
+    }
+
+private:
+    boost::asio::io_context                   & m_io_context;
+
+    std::atomic_size_t                          m_cur_size;
+    size_t                                      m_max_size;
+    std::vector<proxy_server_6::detail::bucket> m_data;
+    proxy_server_6::detail::command_queue       m_command_by_time;
+    bool                                        m_verbose;
+};
+
+
+explicit cache::cache( boost::asio::io_context & io_context, size_t bucket_count = 5, std::size_t max_data_size = 1024, bool verbose = false )
+    : m_impl( std::make_unique( io_context, bucket_count, max_data_size, verbose ) )
+{}
+
+
+cache::~cache() = default;
+
+
+std::vector<std::string> cache::get_cached_files()
+{
+    m_impl->get_cached_files();
+}
+
+
+void cache::insert_file( const std::string & command, const std::string & file )
+{
+    m_impl->insert_file(command, file);
 }
 
 
 bool cache::get_file( const std::string & command, std::string & file )
 {
-    bool result = false;
-    size_t command_hash = std::hash<std::string>{}(command);
-    size_t index = command_hash % m_data.size();
-
-    proxy_server_6::cache_locker locker{ m_data[index].m_mtx };
-    if( locker )
-    {
-        file = m_data[index].m_files_by_command[command_hash];
-        result = true;
-    }
-    
-    return result;
-}
-
-
-void cache::delete_oldest_and_insert( const std::string & command, const std::string & file )
-{
-    std::chrono::system_clock::time_point min_time = std::chrono::system_clock::now();
-    proxy_server_6::cache_locker queue_locker_{ m_command_by_time.get_mutex() };
-    size_t oldest_command;
-    if(queue_locker_ && m_command_by_time.empty())
-    {
-        oldest_command = m_command_by_time.get_front_command();
-        m_command_by_time.delete_front_command();
-
-        proxy_server_6::cache_locker data_locker{m_data[oldest_command].m_mtx};
-        auto & bucket = m_data[oldest_command];
-        bucket.m_files_by_command.erase(oldest_command);
-
-        // insert new value
-        size_t command_hash = std::hash<std::string>{}(command);
-        size_t index = command_hash % m_data.size();
-        m_command_by_time.insert( { std::chrono::system_clock::now(), command_hash } );
-        bucket.m_files_by_command.insert( { command_hash, file } );
-        m_cur_size.store( m_cur_size.load( std::memory_order_acquire ) + file.size() );
-    }        
+    m_impl->get_file( command, file );
 }
 
 } // namespace proxy_server_6
--- src/cache.hpp
+++ src/cache.hpp
@@ -1,19 +1,11 @@
 #ifndef CACHE_HPP
 #define CACHE_HPP
 
-#include <atomic>
-#include <chrono>
-#include <future>
-#include <iterator>
-#include <iostream>
-#include <mutex>
-#include <map>
+#include <string>
 #include <vector>
-#include <queue>
-#include <utility>
+#include <memory>
 
 #include <boost/asio.hpp>
-#include <boost/asio/post.hpp>
 
 namespace proxy_server_6
 {
@@ -24,46 +16,28 @@ class cache
 public:
     explicit cache( boost::asio::io_context & io_context, size_t bucket_count = 5, std::size_t max_data_size = 1024, bool verbose = false );
 
-    ~cache() = default;
+    ~cache();
 
 private:
-    cache( cache & other ) = delete;
+    cache( cache & other )             = delete;
     cache & operator=( cache & other ) = delete;
 
-    cache( cache && other ) = delete;
+    cache( cache && other )             = delete;
     cache & operator=( cache && other ) = delete;
 
 public:
 
     std::vector<std::string> get_cached_files();
 
-    void insert( const std::string & command, const std::string & file );
+    void insert_file( const std::string & command, const std::string & file );
 
     bool get_file( const std::string & command, std::string & file );
 
 private:
+    // use pimpl idiom https://en.wikibooks.org/wiki/C%2B%2B_Programming/Idioms#Pointer_To_Implementation_(pImpl)
+    class cache_impl;
 
-    void delete_oldest_and_insert( const std::string & command, const std::string & file );
-
-
-private:
-    class proxy_server_6::command_queue;
-
-    struct bucket
-    {
-        std::mutex                      m_mtx;
-        std::map<size_t, std::string>   m_files_by_command;
-    };
-
-
-    boost::asio::io_context & m_io_context;
-
-    std::atomic_size_t                     m_cur_size;
-    size_t                                 m_max_size;
-    std::vector<bucket>                    m_data;
-    std::unique_ptr<command_queue> m_command_by_time; // pimpl for queue
-
-    bool                      m_verbose;
+    std::unique_ptr<cache_impl> m_impl; // pimpl for queue
 };
 
 }   // namespace proxy_server_6
