94a3adee53511891b56c7c548b9877e897b266bc
NodiraFazylova <nshorakhmedova@gmail.com>
2021-03-16 23:03:11 +0500

    Add some error codes

---
+++ src/error.hpp
@@ -0,0 +1,17 @@
+#ifndef ERROR_HPP
+#define ERORR_HPP
+
+namespace error
+{
+
+enum class errc : int32_t
+{
+    not_error           = 0x00000000,
+    unknown_error       = 0x00000001,
+
+    file_too_large      = 0x50000001
+};
+
+}   // namespace error
+
+#endif  // ERROR_HPP
