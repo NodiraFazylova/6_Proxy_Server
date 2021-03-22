f956557e91bb8e08dfaa37073807699895042356
NodiraFazylova <nshorakhmedova@gmail.com>
2021-03-23 01:21:11 +0500

    Add new run io context class

--- src/utils/run_io_context.hpp
+++ src/utils/run_io_context.hpp
@@ -50,6 +50,41 @@ io_run_context_t<T> run_io_context( T & io_context )
 	return io_run_context_t( io_context );
 }
 
+template <typename T>
+class io_run_context_once_t
+{
+public:
+	io_run_context_once_t( T & io_context )
+		: m_io_context( io_context )
+	{}
+
+	void operator()() noexcept
+	{
+		try
+		{
+			m_io_context.run(); // block here
+		}
+		catch( std::exception & e )
+		{
+			LOG_ERROR( "run_io_context_t(): EXCEPTION: {0}", e.what() );
+		}
+		catch( ... )
+		{
+			LOG_ERROR( "run_io_context_t(): UNKNOWN EXCEPTION" );
+		}
+	}
+
+private:
+	T & m_io_context;
+};
+
+
+template <typename T>
+io_run_context_once_t<T> io_run_context_once( T & io_context )
+{
+	return io_run_context_once_t( io_context );
+}
+
 }	// proxy_server_6
 
 #endif	/* RUN_IO_CONTEXT_HPP */
